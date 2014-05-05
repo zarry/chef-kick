d4cc6addd85c1b2bfafb60e1ff55ef97207f87b0
Larry Zarou <lzarou@commercehub.com>
2014-05-05 17:50:19 -0400

    fixing chef-kick to return proper exit code on exit

--- src/chef-kick/c/chef-kick.c
+++ src/chef-kick/c/chef-kick.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
    setuid(0);
-   return system("/usr/bin/chef-client --once");
+   int exitCode = system("/usr/bin/chef-client --once");
+   return(WEXITSTATUS(exitCode));
 }
